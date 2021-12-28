4721ca5c345196ec35b539fe0d3bd3f4e30e9871
Andrei Ionescu <voidwarden@yahoo.com>
2021-12-28 23:48:43 +0200

    Create empty database

---
+++ Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace KixPlay_Backend.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) 
+            : base(options)
+        {
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,22 @@
+using KixPlay_Backend.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Configure the connection to the database by specifying the connection string
+builder.Services.AddDbContext<DataContext>(contextOptions =>
+{
+    contextOptions.UseSqlServer(
+        builder.Configuration.GetConnectionString("KixPlayDatabaseConnection")
+    );
+});
 
 var app = builder.Build();
 
